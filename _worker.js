3f306a59051b7703e09c88395a7b427ccbbfa6a7
parseh442 <parseh442@gmail.com>
2024-09-18 20:24:17 +0330

    Update _worker255.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ae84a980-efff-4e12-8c0e-ed0bbc1f4ef8';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
